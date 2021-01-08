76df70b5f75bec337984e38485869a160beda32a
ripee <ripee.dev@gmail.com>
2021-01-08 20:49:54 +0100

    tre3calteskt: Move RR to its own branch

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,11 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_tre3calteskt.mk \
-    $(LOCAL_DIR)/rr_tre3calteskt.mk
+    $(LOCAL_DIR)/lineage_tre3calteskt.mk
     
 COMMON_LUNCH_CHOICES := \
     lineage_tre3calteskt-user \
     lineage_tre3calteskt-userdebug \
-    lineage_tre3calteskt-eng \
-    rr_tre3calteskt-userdebug
+    lineage_tre3calteskt-eng
