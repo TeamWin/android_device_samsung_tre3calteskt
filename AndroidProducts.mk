c0a3375f4e5bf757202fd54b634e1864d0a3fc88
Anan Jaser <ananjaser@gmail.com>
2021-06-29 06:52:33 +0400

    tre3calteskt: RR-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2021 The Ressurection Remix Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_tre3calteskt.mk
+    $(LOCAL_DIR)/rr_tre3calteskt.mk
     
 COMMON_LUNCH_CHOICES := \
-    lineage_tre3calteskt-user \
-    lineage_tre3calteskt-userdebug \
-    lineage_tre3calteskt-eng
+    rr_tre3calteskt-user \
+    rr_tre3calteskt-userdebug \
+    rr_tre3calteskt-eng
